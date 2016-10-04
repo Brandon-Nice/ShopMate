42497af18b7d92f098b89725d9e8a87f76cea455
Will Enright <wenright@purdue.edu>
2016-10-04 18:06:20 -0400

    Fix example list so that it correctly displays
    Currently using dummy data. Eventually will pull information from database and set the information there.

--- app/src/main/java/com/shopmate/shopmate/ShoppingListActivity.java
+++ app/src/main/java/com/shopmate/shopmate/ShoppingListActivity.java
@@ -6,9 +6,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -34,35 +36,44 @@ public class ShoppingListActivity extends AppCompatActivity {
 
         // These are just some mock items to add to the list.
         // TODO retrieve these items from the database
-        final List<String> items = new ArrayList<String>();
+        List<String> items = new ArrayList<String>();
         for (int i = 0; i < 10; i ++) {
             items.add("Food " + i);
         }
 
-        final ListView shoppingList = (ListView) findViewById(R.id.shoppingList);
-        final ShoppingListItemAdapter shoppingListItemAdapter = new ShoppingListItemAdapter(this, R.layout.shopping_list_item, items);
+        ListView shoppingList = (ListView) findViewById(R.id.shoppingList);
+        ShoppingListItemAdapter shoppingListItemAdapter = new ShoppingListItemAdapter(this, R.layout.shopping_list_item, items);
         shoppingList.setAdapter(shoppingListItemAdapter);
-
-
     }
 
     private class ShoppingListItemAdapter extends ArrayAdapter<String> {
         // TODO create an item class that contains things like price, quantity, brand, etc.
         private List<String> items;
+        private Context context;
 
         public ShoppingListItemAdapter(Context context, int resourceId, List<String> items) {
             super(context, resourceId, items);
             this.items = items;
+            this.context = context;
         }
-//
-//        @Override
-//        public long getItemId(int position) {
-//            return position;
-//        }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            // TODO
+            View view;
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater)
+                        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.shopping_list_item, null);
+            } else {
+                view = convertView;
+            }
+
+            String itemName = items.get(position);
+
+            CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkBox);
+            checkBox.setText(itemName);
+
+            return view;
         }
     }
 
