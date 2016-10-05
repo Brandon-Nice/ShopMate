b588af09fe4576d09f435a19e5ec75e1a293c151
Will Enright <wenright@purdue.edu>
2016-10-04 20:45:34 -0400

    Add create new item button

--- app/src/main/java/com/shopmate/shopmate/ShoppingListActivity.java
+++ app/src/main/java/com/shopmate/shopmate/ShoppingListActivity.java
@@ -3,7 +3,6 @@ package com.shopmate.shopmate;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -25,25 +24,28 @@ public class ShoppingListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "This action will normally add a new item to the shopping list", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         // These are just some mock items to add to the list.
         // TODO retrieve these items from the database
-        List<String> items = new ArrayList<String>();
+        final List<String> items = new ArrayList<String>();
         for (int i = 0; i < 10; i ++) {
             items.add("Food " + i);
         }
 
+        // Creates an adapter which is used maintain and render a list of items
         ListView shoppingList = (ListView) findViewById(R.id.shoppingList);
-        ShoppingListItemAdapter shoppingListItemAdapter = new ShoppingListItemAdapter(this, R.layout.shopping_list_item, items);
+        final ShoppingListItemAdapter shoppingListItemAdapter = new ShoppingListItemAdapter(this, R.layout.shopping_list_item, items);
         shoppingList.setAdapter(shoppingListItemAdapter);
+
+        // Adds a new item to this shopping list when the button is pressed
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO Either bring user to new item activity or allow user to enter item info here
+                items.add("New Food Item");
+                shoppingListItemAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     private class ShoppingListItemAdapter extends ArrayAdapter<String> {
@@ -61,8 +63,7 @@ public class ShoppingListActivity extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             View view;
             if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater)
-                        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 view = inflater.inflate(R.layout.shopping_list_item, null);
             } else {
                 view = convertView;
