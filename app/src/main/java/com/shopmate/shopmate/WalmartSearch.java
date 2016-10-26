b2f10e6b7f6db71f8b6d40fe37d8ed60bffedffe
Brandon Nice <niceb@purdue.edu>
2016-10-26 15:40:33 -0400

    App is now able to parse Walmart searches. TODO: add the search result into an Item array, then send that to a results activity where the user can select the correct item. After, it adds it to the list they selected.

--- app/src/main/java/com/shopmate/shopmate/WalmartSearch.java
+++ app/src/main/java/com/shopmate/shopmate/WalmartSearch.java
@@ -1,12 +1,15 @@
 package com.shopmate.shopmate;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,7 +25,7 @@ import java.io.InputStreamReader;
 public class WalmartSearch extends AppCompatActivity {
 
     public static EditText walmartEditText;
-
+    public String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,51 +37,18 @@ public class WalmartSearch extends AppCompatActivity {
             public void onClick(View v) {
                 walmartEditText = (EditText) findViewById(R.id.walmartEditText);
                 String searchTerms = walmartEditText.getText().toString();
-                if(searchTerms != null || !searchTerms.equals(" ")) {
-                    //TODO: Search the Walmart API database and populate the listview of results.
-                    //use gson to parse walmart api returns
-                    //TODO: Extra features: category, sorting (increasing/decreasing price, relevant, bestsellers)
-
-                    //Prepare the URL
-                    String url = "http://api.walmartlabs.com/v1/search?query=";
-                    String sort = "";
-                    String order = "";
-
-                    //Add the search terms to the query url
-                    searchTerms = searchTerms.replace(" ","");
-                    url += searchTerms;
-
-                    //Add the format and apiKey
-                    url += "&format=json";
-                    url += "&apiKey=hk42mtbf4g9y59gvb4z4swuu";
-
-                    /*
-                    if sort changed from relevance to price, title, bestseller, customerrating, new, add it to the search query
-                    if order is changed from asc to desc, add it to the search query (only needed for price, title, customerrating)
-                     */
-                    System.out.println("URL:" + url);
-                    //Use URLConnection to connect with walmart
-                    try {
-                        JSONObject jsonObject = getJSONObjectFromURL(url);
-                        //TODO: parse json object
-                    }
-                    catch (JSONException e) {
-                        //Toast message saying bad URL, try again
-                        System.out.println("Bad URL");
-                        e.printStackTrace();
-                    }
-                    catch (IOException e) {
-                        //Toast message saying unknown error, try again
-                        System.out.println("IO Exception");
-                        e.printStackTrace();
-                    }
+                if (searchTerms != null || !searchTerms.equals(" ")) {
+                    new JSONParse().execute();
                 }
                 else {
-                    //TODO: Send a toast message saying to please enter a search value
+                    //Toast message saying to enter a field in the textbox
                 }
             }
+
         });
     }
+
+
     public static JSONObject getJSONObjectFromURL(String urlString) throws IOException, JSONException {
 
         HttpURLConnection urlConnection = null;
@@ -113,5 +83,63 @@ public class WalmartSearch extends AppCompatActivity {
         System.out.println("JSON: " + jsonString);
 
         return new JSONObject(jsonString);
+    }
+
+    public class JSONParse extends AsyncTask<String, String, JSONObject> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            String searchTerms = walmartEditText.getText().toString();
+                //TODO: Search the Walmart API database and populate the listview of results.
+                //use gson to parse walmart api returns
+                //TODO: Extra features: category, sorting (increasing/decreasing price, relevant, bestsellers)
+
+                //Prepare the URL
+                url = "http://api.walmartlabs.com/v1/search?query=";
+                String sort = "";
+                String order = "";
+
+                //Add the search terms to the query url
+                searchTerms = searchTerms.replace(" ", "");
+                url += searchTerms;
+
+                //Add the format and apiKey
+                url += "&format=json";
+                url += "&apiKey=hk42mtbf4g9y59gvb4z4swuu";
+
+                    /*
+                    if sort changed from relevance to price, title, bestseller, customerrating, new, add it to the search query
+                    if order is changed from asc to desc, add it to the search query (only needed for price, title, customerrating)
+                     */
+                System.out.println("URL:" + url);
+
+        }
+        @Override
+        protected JSONObject doInBackground(String... args) {
+
+            try {
+                JSONObject json = getJSONObjectFromURL(url);
+                    JSONArray jsonArray = json.getJSONArray("items");
+                    System.out.println(jsonArray.toString());
+                    for(int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject item = jsonArray.getJSONObject(i);
+                        String name = item.getString("name");
+                        Double price = item.getDouble("salePrice");
+                        System.out.println(name + ": " + price);
+
+                        //TODO: Create an item for each entry (add it to an item array) and then send it to a
+                        //TODO:  results activity. When a user clicks on that item, it will give them more information.
+                        //TODO:  then the user has the option of adding it to their cart. If they do, it will be added.
+                    }
+                return json;
+            }
+            catch(JSONException e) {
+                e.printStackTrace();
+            }
+            finally {
+                return null;
+            }
+        }
     }
 }
