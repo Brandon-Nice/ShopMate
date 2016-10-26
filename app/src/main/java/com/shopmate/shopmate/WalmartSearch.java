a983e43fc2c39b1ec4a681369d981e2559456193
Brandon Nice <niceb@purdue.edu>
2016-10-25 22:45:45 -0400

    Started working on the AddItemActivity. So far has a mock UI, add button currently does not work. The search Walmart button will take the user to a new activity. Search button does nothing so far, but will go to Walmart lookup and populate the result listView. Walmart Search currently crashes due to the https connection being on the main tread. Need to change to either a thread or AsyncTask.

--- app/src/main/java/com/shopmate/shopmate/WalmartSearch.java
+++ app/src/main/java/com/shopmate/shopmate/WalmartSearch.java
@@ -7,6 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class WalmartSearch extends AppCompatActivity {
 
     public static EditText walmartEditText;
@@ -21,10 +33,85 @@ public class WalmartSearch extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 walmartEditText = (EditText) findViewById(R.id.walmartEditText);
-                if(walmartEditText.getText() != null) {
+                String searchTerms = walmartEditText.getText().toString();
+                if(searchTerms != null || !searchTerms.equals(" ")) {
                     //TODO: Search the Walmart API database and populate the listview of results.
+                    //use gson to parse walmart api returns
+                    //TODO: Extra features: category, sorting (increasing/decreasing price, relevant, bestsellers)
+
+                    //Prepare the URL
+                    String url = "http://api.walmartlabs.com/v1/search?query=";
+                    String sort = "";
+                    String order = "";
+
+                    //Add the search terms to the query url
+                    searchTerms = searchTerms.replace(" ","");
+                    url += searchTerms;
+
+                    //Add the format and apiKey
+                    url += "&format=json";
+                    url += "&apiKey=hk42mtbf4g9y59gvb4z4swuu";
+
+                    /*
+                    if sort changed from relevance to price, title, bestseller, customerrating, new, add it to the search query
+                    if order is changed from asc to desc, add it to the search query (only needed for price, title, customerrating)
+                     */
+                    System.out.println("URL:" + url);
+                    //Use URLConnection to connect with walmart
+                    try {
+                        JSONObject jsonObject = getJSONObjectFromURL(url);
+                        //TODO: parse json object
+                    }
+                    catch (JSONException e) {
+                        //Toast message saying bad URL, try again
+                        System.out.println("Bad URL");
+                        e.printStackTrace();
+                    }
+                    catch (IOException e) {
+                        //Toast message saying unknown error, try again
+                        System.out.println("IO Exception");
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    //TODO: Send a toast message saying to please enter a search value
                 }
             }
         });
+    }
+    public static JSONObject getJSONObjectFromURL(String urlString) throws IOException, JSONException {
+
+        HttpURLConnection urlConnection = null;
+
+        URL url = new URL(urlString);
+
+        urlConnection = (HttpURLConnection) url.openConnection();
+
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setReadTimeout(10000 /* milliseconds */);
+        urlConnection.setConnectTimeout(15000 /* milliseconds */);
+
+        urlConnection.setDoOutput(true);
+
+        urlConnection.connect();
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(url.openStream()));
+
+        char[] buffer = new char[1024];
+
+        String jsonString = new String();
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line+"\n");
+        }
+        br.close();
+
+        jsonString = sb.toString();
+
+        System.out.println("JSON: " + jsonString);
+
+        return new JSONObject(jsonString);
     }
 }
