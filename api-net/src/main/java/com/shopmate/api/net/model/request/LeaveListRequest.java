45cd3dbc7b4057341b1e4751cca0c5af0d2e3bf6
Aaron Dierking <aarondierking7@gmail.com>
2016-11-30 21:31:18 -0500

    API method for leaving a list

---
+++ api-net/src/main/java/com/shopmate/api/net/model/request/LeaveListRequest.java
@@ -0,0 +1,7 @@
+package com.shopmate.api.net.model.request;
+
+public class LeaveListRequest extends AuthenticatedRequest {
+    public LeaveListRequest(String fbToken) {
+        super(fbToken);
+    }
+}
